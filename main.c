0fb138c2a35be67c997f49b31c058d98cd1a4655
Regan-Koopmans <regankoopmans@protonmail.com>
2017-09-19 18:40:23 +0200

    Preparing for demo.

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 # include <stdio.h>
 # include <string.h>
+# include <unistd.h>
 
 int main(int argc, char ** argv) {
-    char buffer[500];
+    char buffer[1024];
+    setuid(0);
+    printf("\n################################\n");
+    printf("# THE UNIVAC INSECURITY SYSTEM #\n");
+    printf("#    By Ernst and Regan.       #\n");
+    printf("################################\n\n");
     strcpy(buffer, argv[1]);
+    printf("You entered the text : %s\n\n", buffer);
     return 0;
 }
